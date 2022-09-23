3b7d909fc68685b0a19b12f65d4b0d42e04ede18
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-09-23 15:27:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
-  const input = "Hey !";
   return (
-    <Welcome name={input} />
+    <Welcome name="Hey !" />
   )
 }
 
