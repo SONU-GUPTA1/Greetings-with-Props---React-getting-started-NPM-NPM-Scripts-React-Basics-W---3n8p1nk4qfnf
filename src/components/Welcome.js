be9185161ffdae27a8ae32226e3f319a54553ee4
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-09-23 13:28:42 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,8 +2,9 @@
 function Welcome(){
   return(
     <div>
-    <h1> Hey !</h1>
-    <h2> Welcome to Newton School.</h2>
+    <h1> Hey ! </h1>
+    <h2> Welcome to Newton School. </h2>
     </div>
     )
 }
+export default Welcome
