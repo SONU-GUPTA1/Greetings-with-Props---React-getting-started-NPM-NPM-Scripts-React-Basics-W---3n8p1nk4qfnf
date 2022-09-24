20f887ffb71e0197e853d069004e8c1882d05676
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-09-24 12:39:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  const[input, setInput ] = useState(" ");
+  const change=(e)=>{
+    setInput(e.target.value);
+  }
   return (
-    <Welcome name="Hey !" />
+    <div>
+    <input type="text" onChange={change} /> 
+      <Welcome name={input} />
+    </div>
   )
 }
 
